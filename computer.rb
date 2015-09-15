9ae7f6ce7c359e04270219027e0e85459833ed34
Temitope <lytopz@gmail.com>
2015-09-15 12:40:57 +0100

    Just made some changes

--- computer.rb
+++ computer.rb
@@ -1,19 +1,40 @@
 class Computer 
 
-  def self.beginner
-
-     color_combination = "(v)iolet, (g)reen, (y)ellow and (r)ed.";
+  def self.begin(color_code)
+     color_combination = self.convert_code_to_color(color_code)
       Message.new("I have generated a beginner sequence with four elements made up of:
-    #{color_combination} You are to guess the sequence in which these colors appeared e.g VGYR for (v)iolet, (g)reen, (y)ellow and (r)ed. You have
-    12 guesses to get these colors or you lose the game. Use (q)uit at any time to end the game.")
+      #{color_combination} You are to guess the sequence in which these colors appeared e.g RGBY for (r)ed, (g)reen, (y)ellow and (g)reen. You have 12 guesses to get these colors or you lose the game. Use (q)uit at any time to end the game.")
     Message.new("Ready to play?")
     Message.new("What's your guess?")
+  end
+
+  def self.convert_code_to_color(code)
+    str_concat = ""
+    if !code.empty?
+      code.each { |c|
+      case c
+      when "R"
+      str_concat+="(R)ed".colorize(:red) + ", "
+      when "G"
+      str_concat+="(G)reen".colorize(:green) + ", "
+      when "B"
+      str_concat+="(B)lue".colorize(:blue) + ", "
+      when "Y"
+      str_concat+="(Y)ellow".colorize(:yellow) + ", "
+      when "V"
+      str_concat+="(V)iolet".colorize(:violet) + ", "
+     when "I"
+      str_concat+="(I)ndigo".colorize(:indigo) + ", "
+     end
+      }
+    end
+    return str_concat
   end
 
   def self.beginner_code
     colors = ["R", "G", "B", "Y"]
      array = []
-     4.times { array << rand(5)}
+     4.times { array << rand(4)}
      generated_colors = array.collect { |i| colors[i] }
      return generated_colors
   end
@@ -32,7 +53,38 @@ class Computer
      8.times { array << rand(6)}
      generated_colors = array.collect { |i| colors[i] }
      return generated_colors
-
   end
 
+  def self.partial_and_matches(comp,player)
+     player = player.upcase.split("")
+     all = comp.zip(player)
+     comp_array = []
+     user_array = []
+     matches = 0
+     all.each do |a,b|
+       if a===b 
+       matches+=1
+       else
+       comp_array << a
+       user_array << b
+      end
+    end
+    comp_hash = Hash.new 0
+      comp_array.each do |word|
+      comp_hash[word] += 1
+    end
+    user_hash = Hash.new 0
+      user_array.each do |word|
+      user_hash[word] += 1
+    end
+    result =0
+    user_hash.each do |key, value|
+      if comp_hash[key] >= value
+        result +=value
+      else
+        result +=comp_hash[key]
+      end
+   end
+    return "#{matches} exact match and #{result} partial found"
+ end 
 end
--- play_game.rb
+++ play_game.rb
@@ -32,11 +32,34 @@ class InitializeGame
 	end
 
     def play_game
-      display_player_level # call the display on the player level in order to know which code to generate for the player
-      guesses = 12
-      Message.new("So Would you like to play?".colorize(:green))
-      Message.new("So start by telling me your name:".colorize(:green))
-      player = gets.chomp
+       computer_generated_color  = "" # capture the computer generated code here
+       display_player_level # call the display on the player level in order to know which code to generate for the player
+       @level = gets.chomp
+       level = Player.choose_game_level(@level) # This returns the level to which the player to  play
+       level_keys = [1,2,3]
+      if level_keys.include? level.to_i 
+         if level == 1
+           computer_generated_color = Computer.beginner_code # The beginner generated color codes in array
+           elsif level == 2
+           computer_generated_color = Computer.intermediate_code # The Intermediate generated color codes in array
+           elsif level == 3
+           computer_generated_color = Computer.advanced_code # The Advanced generated color codes in array
+         end
+        Message.new("So Would you like to play?".colorize(:green))
+        Message.new("So start by telling me your name:".colorize(:green))
+        player = gets.chomp
+        Computer.begin(computer_generated_color.shuffle)
+        #get the user guess and the guess must not be greater than the length of the computer generated code
+        player_guess = gets.chomp # get the player guess
+        player_guess = player_guess.upcase #convert the player guess to upcase 
+        guesses = 12 # Number of chances the user has before starting the game
+        puts Computer.partial_and_matches(computer_generated_color,player_guess)
+
+       else
+        Message.new("You have entered a wrong level option") #output a message to the user concerning a wrong input
+        self.play_game # call the init level agian when the user input a wrong input
+        end
+      end
 
     end
 
@@ -56,22 +79,6 @@ class InitializeGame
 	  Message.new(beginner_info)
 	  Message.new(intermediate_info)
 	  Message.new(advanced_info)
-    @level = gets.chomp
-    level = Player.choose_game_level(@level)
-    level_keys = [1,2,3]
-    if level_keys.include? level.to_i 
-         if level == 1
-         elsif level == 2
-          Message.new("level 2")
-         elsif level == 3
-           Message.new("level 3")
-         end
-       else
-        Message.new("You have entered a wrong level option") #output a message to the user concerning a wrong input
-        self.display_player_level # call the init level agian when the user input a wrong input
-      end
-	end
-
 end
 
 Play = InitializeGame.new
