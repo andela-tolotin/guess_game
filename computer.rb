2ae27adad1aa3f5bbbea57dce55501449e911f1d
Temitope <lytopz@gmail.com>
2015-09-17 08:14:36 +0100

    Latest files added to my Mastermind game files

--- computer.rb
+++ computer.rb
@@ -1,8 +1,8 @@
 class Computer 
 
   def self.begin(color_code)
-     color_combination = self.convert_code_to_color(color_code)
-     #determine the player level by their color code
+    color_combination = self.convert_code_to_color(color_code)
+    #determine the player level by their color code
     game_level = color_code.length.to_i
     game_info = ""
     if game_level == 4
@@ -12,7 +12,7 @@ class Computer
     else
       game_info = "I have generated a sequence with six elements made up of"
     end
-      Message.new("#{game_info} : #{color_combination} You are to guess the sequence in which these colors appeared e.g RGBY for (r)ed, (g)reen, (y)ellow and (g)reen. You have 12 guesses to get these colors or you lose the game. Use (q)uit at any time to end the game.")
+    Message.new("#{game_info} : #{color_combination} You are to guess the sequence in which these colors appeared e.g RGBY for (r)ed, (g)reen, (y)ellow and (g)reen. You have 12 guesses to get these colors or you lose the game. Use (q)uit at any time to end the game.")
     Message.new("Ready to play?")
     Message.new("What's your guess?")
   end
@@ -42,40 +42,40 @@ class Computer
 
   def self.beginner_code
     colors = ["R", "G", "B", "Y"]
-     array = []
-     4.times { array << rand(4)}
-     generated_colors = array.collect { |i| colors[i] }
-     return generated_colors
+    array = []
+    4.times { array << rand(4)}
+    generated_colors = array.collect { |i| colors[i] }
+    return generated_colors
   end
 
   def self.intermediate_code
-     colors = ["R", "G", "B", "Y", "V"]
-     array = []
-     5.times { array << rand(5)}
-     generated_colors = array.collect { |i| colors[i] }
-     return generated_colors
+    colors = ["R", "G", "B", "Y", "V"]
+    array = []
+    5.times { array << rand(5)}
+    generated_colors = array.collect { |i| colors[i] }
+    return generated_colors
   end
 
   def self.advanced_code
     colors = ["R", "G", "B", "Y", "V","I"]
-     array = []
-     6.times { array << rand(6)}
-     generated_colors = array.collect { |i| colors[i] }
-     return generated_colors
+    array = []
+    6.times { array << rand(6)}
+    generated_colors = array.collect { |i| colors[i] }
+    return generated_colors
   end
 
   def self.partial_and_matches(comp,player)
-     player = player.upcase.split("")
-     all = comp.zip(player)
-     comp_array = []
-     user_array = []
-     matches = 0
-     all.each do |a,b|
-       if a===b 
-       matches+=1
-       else
-       comp_array << a
-       user_array << b
+    player = player.upcase.split("")
+    all = comp.zip(player)
+    comp_array = []
+    user_array = []
+    matches = 0
+    all.each do |a,b|
+      if a===b 
+        matches+=1
+      else
+        comp_array << a
+        user_array << b
       end
     end
     comp_hash = Hash.new 0
--- player.rb
+++ player.rb
@@ -3,37 +3,36 @@ class Player
 attr_accessor:player
 
 	def self.player_name(player_name)
-		@player = player_name
-		@player
+	  @player = player_name
+	  @player
 	end
 
 	def self.choose_game_mode(game_mode)
-		@game_mode = game_mode.downcase
-		if !@game_mode.nil?
-			case @game_mode
-			when "p"
-			  return "p"
-			when "i"
-			  return "i"
-			when "q"
-			  return "q"
-			when "b"
-			  return "b"
-			else
-              
+	  @game_mode = game_mode.downcase
+	  if !@game_mode.nil?
+		case @game_mode
+		  when "p"
+		    return "p"
+		  when "i"
+		    return "i"
+		  when "q"
+		    return "q"
+		  when "b"
+		    return "b"
+	  else
 			end
 		end
 	end
 
 	def self.choose_game_level(player_level)
-		@p_level = player_level.to_i
-		if @p_level == 1
-			return 1
-		  elsif @p_level == 2
-			return 2
-		  elsif @p_level == 3
-		  	return 3
-		end		
+	  @p_level = player_level.to_i
+	  if @p_level == 1
+		return 1
+	  elsif @p_level == 2
+	    return 2
+	  elsif @p_level == 3
+		return 3
+	  end		
 	end
 
 end
